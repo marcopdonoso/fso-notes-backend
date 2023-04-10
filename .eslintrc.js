238b71b081205450f09e12141afaae07cc8642d7
Marco Perez Donoso <marcopdonoso@gmail.com>
2023-04-10 10:48:01 -0400

    adding prettier config to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,18 @@
 module.exports = {
-	"env": {
-		"commonjs": true,
-		"es2021": true,
-		"node": true
+	env: {
+		commonjs: true,
+		es2021: true,
+		node: true,
 	},
-	"extends": "eslint:recommended",
-	"overrides": [
-	],
-	"parserOptions": {
-		"ecmaVersion": "latest"
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: "latest",
 	},
-	"rules": {
-		"indent": [
-			"error",
-			"tab"
-		],
-		"linebreak-style": [
-			"error",
-			"unix"
-		],
-		"quotes": [
-			"error",
-			"double"
-		],
-		"semi": [
-			"error",
-			"always"
-		]
-	}
+	rules: {
+		indent: ["error", "tab"],
+		"linebreak-style": ["error", "unix"],
+		quotes: ["error", "double"],
+		semi: ["error", "always"],
+	},
+	extends: ["eslint:recommended", "prettier"],
 };
