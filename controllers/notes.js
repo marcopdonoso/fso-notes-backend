dcd7ccb6d61a9dded7d9dded1275ed2f3aee233f
Marco Perez Donoso <marcopdonoso@gmail.com>
2023-04-12 12:14:18 -0400

    error handler and async/await

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,10 +1,9 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 
-notesRouter.get('/', (request, response) => {
-	Note.find({}).then(notes => {
-		response.json(notes)
-	})
+notesRouter.get('/', async (request, response) => {
+	const notes = await Note.find({})
+	response.json(notes)
 })
 
 notesRouter.get('/:id', (request, response, next) => {
@@ -19,7 +18,7 @@ notesRouter.get('/:id', (request, response, next) => {
 		.catch(error => next(error))
 })
 
-notesRouter.post('/', (request, response, next) => {
+notesRouter.post('/', async (request, response, next) => {
 	const body = request.body
 
 	const note = new Note({
@@ -27,13 +26,12 @@ notesRouter.post('/', (request, response, next) => {
 		important: body.important || false,
 		date: new Date(),
 	})
-
-	note
-		.save()
-		.then(savedNote => {
-			response.json(savedNote)
-		})
-		.catch(error => next(error))
+	try {
+		const savedNote = await note.save()
+		response.json(savedNote)
+	} catch (exception) {
+		next(exception)
+	}
 })
 
 notesRouter.delete('/:id', (request, response, next) => {
