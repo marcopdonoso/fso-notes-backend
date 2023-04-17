918ae3b36b7cd80d74c7ed33211495f8ec09b15d
Marco Perez Donoso <marcopdonoso@gmail.com>
2023-04-17 16:30:09 -0400

    users administration

---
+++ tests/user_api.test.js
@@ -0,0 +1,48 @@
+const supertest = require('supertest')
+const mongoose = require('mongoose')
+const helper = require('./test_helper')
+const app = require('../app')
+const api = supertest(app)
+const bcrypt = require('bcrypt')
+
+const User = require('../models/user')
+
+describe('when there is initially one user in DB', () => {
+	beforeEach(async () => {
+		await User.deleteMany({})
+
+		const passwordHash = await bcrypt.hash('secret', 10)
+		const user = new User({
+			username: 'root',
+			passwordHash,
+		})
+
+		await user.save()
+	}, 100000)
+
+	test('creation succeeds with a fresh username', async () => {
+		const usersAtStart = await helper.usersInDb()
+
+		const newUser = {
+			username: 'marcopdonoso',
+			name: 'Marco Perez Donoso',
+			password: '123',
+		}
+
+		await api
+			.post('/api/users')
+			.send(newUser)
+			.expect(200)
+			.expect('Content-type', /application\/json/)
+
+		const usersAtEnd = await helper.usersInDb()
+		expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+		const usernames = usersAtEnd.map(u => u.username)
+		expect(usernames).toContain(newUser.username)
+	})
+})
+
+afterAll(() => {
+	mongoose.connection.close()
+})
