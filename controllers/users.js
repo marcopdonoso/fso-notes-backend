9b61a7101201fb45d3496b34715dc60f8cb517f2
Marco Perez Donoso <marcopdonoso@gmail.com>
2023-04-18 12:07:26 -0400

    creating users and populating notes field in get /api/users route and user field in get /api/notes route

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,11 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
+usersRouter.get('/', async (request, response) => {
+	const users = await User.find({}).populate('notes', { content: 1, date: 1 })
+	response.json(users)
+})
+
 usersRouter.post('/', async (request, response) => {
 	const body = request.body
 
